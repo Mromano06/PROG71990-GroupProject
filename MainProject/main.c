b9c38bba9fe789dcfa48584052f45bd7e6087aad
Mromano06 <matbat2205@gmail.com>
2024-11-30 17:31:36 -0500

    File will now be created wether or not it already exists

--- MainProject/main.c
+++ MainProject/main.c
@@ -1,4 +1,7 @@
+#include "product.h"
+#include "productList.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 // group4 - 71990f24 - group project
 
@@ -7,6 +10,26 @@
 // Main implementation for group project
 
 int main(void) {
+
+	FILE* originalFile;														// made temp files to create them
+	FILE* newFile;
+	const char* originalFileName = "flightData.txt";						// saved the file names as const chars
+	const char* newFileName = "flightDataUpdated.txt";
+
+	newFile = fopen(newFileName, "w");										// creates and adds new files
+	if (newFile == NULL) {													// (also error checks)
+		fprintf(stderr, "system was unable to create file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// creates new file to print to (if needed)
+	originalFile = fopen(originalFileName, "r");
+	if (originalFile == NULL) {
+		originalFile = fopen(originalFileName, "w");
+	}
+
+	fclose(originalFile);
+	fclose(newFile);
 
 
 	return 0;
